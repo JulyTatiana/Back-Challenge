c9adf3e9b56fab830c2f29361d5d42b36ff7c276
julytatiana <yulytatiana@live.com>
2022-06-08 16:46:42 -0500

    Provider creation

---
+++ src/main/java/com/sofka/demo/collections/Provider.java
@@ -0,0 +1,22 @@
+package com.sofka.demo.collections;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+
+@Document(collection = "provider")
+@Data
+public class Provider {
+
+    @Id
+    private String providerId;
+    @NotBlank(message = "Provider name is empty")
+    private String providerName;
+    @NotBlank(message = "Provider id is empty")
+    private String providerIdentification;
+    @NotBlank(message = "Description is empty")
+    private String providerDescription;
+
+}
