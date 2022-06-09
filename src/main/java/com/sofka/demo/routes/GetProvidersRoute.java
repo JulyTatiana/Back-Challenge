6a02fac07f4f900132fedc1b2bfbed8bf501fc37
julytatiana <yulytatiana@live.com>
2022-06-08 22:10:58 -0500

    CreateProviderRoute was updated

--- src/main/java/com/sofka/demo/routes/GetProvidersRoute.java
+++ src/main/java/com/sofka/demo/routes/GetProvidersRoute.java
@@ -5,6 +5,7 @@ import com.sofka.demo.usecases.GetProvidersUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -21,6 +22,7 @@ public class GetProvidersRoute {
 
     public RouterFunction<ServerResponse> allProviders(GetProvidersUseCase getProvidersUseCase){
         return route(GET("/get/providers"), request -> ServerResponse.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromProducer(getProvidersUseCase.getAllProviders(), ProviderDTO.class)));
     }
 }
