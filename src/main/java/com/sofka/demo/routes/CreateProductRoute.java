d801ab6121526eddcc6a4095c88fe0f3b718904f
julytatiana <yulytatiana@live.com>
2022-06-12 17:58:54 -0500

    CreateProductUseCase was added

---
+++ src/main/java/com/sofka/demo/routes/CreateProductRoute.java
@@ -0,0 +1,27 @@
+package com.sofka.demo.routes;
+
+import com.sofka.demo.dtos.DTOproduct;
+import com.sofka.demo.useCases.CreateProductUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class CreateProductRoute {
+
+    @Bean
+    public RouterFunction<ServerResponse> newProduct(CreateProductUseCase createProductUseCase){
+        return route(POST("/create/product").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(DTOproduct.class)
+                        .flatMap(DTOproduct -> createProductUseCase.createProduct(DTOproduct))
+                        .flatMap(DTOproduct -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(DTOproduct))
+                        .onErrorResume(throwable -> ServerResponse.notFound().build()));
+    }
+}
---
+++ src/main/java/com/sofka/demo/useCases/CreateProductUseCase.java
@@ -0,0 +1,44 @@
+package com.sofka.demo.useCases;
+
+import com.sofka.demo.dtos.DTOproduct;
+import com.sofka.demo.mappers.ProductMapper;
+import com.sofka.demo.repositories.IntProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+@Service
+
+public class CreateProductUseCase {
+
+    private final IntProductRepository repository;
+    private final ProductMapper mapper;
+
+    public CreateProductUseCase(IntProductRepository repository, ProductMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public boolean validateAttributes(DTOproduct productDTO){
+        return productDTO.getProductName() != null &&
+                productDTO.getProductPrice() != null &&
+                productDTO.getProviderId() != null &&
+                productDTO.getAvailableUnits() != null &&
+                productDTO.getMaxQuantity() != null &&
+                productDTO.getMinQuantity() != null;
+    }
+
+    private Mono<DTOproduct> validateProduct(DTOproduct productDTO){
+        return Mono.just(productDTO)
+                .filter(productDTO1 -> validateAttributes(productDTO1))
+                .switchIfEmpty(Mono.error(() -> new Exception("There are missing attributes")));
+    }
+
+    public Mono<DTOproduct> createProduct(DTOproduct productDTO){
+        return validateProduct(productDTO)
+                .flatMap(productDTO1 -> repository.save(mapper.toProduct(productDTO1)))
+                .map(product -> mapper.toProductDTO(product));
+    }
+}
